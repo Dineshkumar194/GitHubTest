bf98ce3954dcedc6880b4cc1c737977a869bf3f6
Dinesh <dinesh.vetrivelu09@gmail.com>
2024-01-27 17:36:06 +1300

    Initial Project Commit

---
+++ githubtest_ddd/src/main/java/githubtest_ddd/Init.java
@@ -0,0 +1,12 @@
+package githubtest_ddd;
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("FIRST LINE");
+		System.out.println("SECOND LINE");
+		
+	}
+
+}
