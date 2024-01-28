ef48f3a03bb29fe85841472456b5d9d12ba967cf
Dineshkumar194 <120300958+Dineshkumar194@users.noreply.github.com>
2024-01-28 18:58:47 +1300

    Update Init.java

--- githubtest_ddd/src/main/java/githubtest_ddd/Init.java
+++ githubtest_ddd/src/main/java/githubtest_ddd/Init.java
@@ -4,6 +4,9 @@ public class Init {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("FEATURE 1 added");
+		
 		System.out.println("FIRST LINE");
 		System.out.println("SECOND LINE");
 		System.out.println("THIRD LINE");
